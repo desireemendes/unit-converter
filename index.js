3a75c80e2045e526bd2cfccc22d156385fa7fae3
Desiree Mendes <desireefrost@live.ca>
2022-11-12 16:52:34 -0500

    add needed measurement variables

--- index.js
+++ index.js
@@ -3,3 +3,18 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+const meterToFeet = 3.281
+const literTGoGal = 0.264
+const kiloToPound = 2.204
+let convertBtn = document.getElementById("convert-btn")
+let length = document.getElementById("length-el")
+
+const getLength = () => {
+    return meter / feet
+}
+
+convertBtn.addEventListener("click", function() {
+    console.log("convert clicked")
+    getLength()
+})
